a6f2c8ff70e81025599b9cc2c9699d16f73defc4
SergiBF <sboronat04@gmail.com>
2022-06-09 13:56:51 +0200

    Falta modificar el repos/service

--- src/main/java/org/it2022/persistencia/PersistenciaTarea.java
+++ src/main/java/org/it2022/persistencia/PersistenciaTarea.java
@@ -8,7 +8,7 @@ import java.util.Date;
 import java.util.List;
 
 @Named
-public class PersistenciaTarea implements PersistenciaTareaInf{
+public class PersistenciaTarea implements PersistenciaTareaInf {
 
     //Patrón Singleton
     private static PersistenciaTarea persi = new PersistenciaTarea();
@@ -25,12 +25,12 @@ public class PersistenciaTarea implements PersistenciaTareaInf{
         ArrayList<Participante> listaParticipantes = new ArrayList<Participante>();
         listaParticipantes.add(new Participante(00, "Pedro", "Martinez", "calle Font", "participante@email.com"));
 
-        this.tarea.add(new Tarea(00, "Cita con el dentista", "20/06/2022", "17:30", agenda, usuario, listaParticipantes ));
+        this.tarea.add(new Tarea(00, "Cita con el dentista", "20/06/2022", "17:30", agenda, usuario, listaParticipantes));
         this.tarea.add(new Tarea(01, "Llevar el coche al taller", "16/06/2022", "18:00", agenda, usuario, listaParticipantes));
     }
 
     //Patrón Singleton
-    public static PersistenciaTarea getInstance(){
+    public static PersistenciaTarea getInstance() {
         return persi;
     }
 
@@ -52,40 +52,57 @@ public class PersistenciaTarea implements PersistenciaTareaInf{
 
     /**
      * Metodo para ontener una tarea a partir de la fecha y el ID del participante
-     * @param fecha le llega un string de fecha
-     * @param IDparticipante le llega el ID de un participante
+     *
+     * @param fecha          le llega un string de fecha
+     * @param idTarea le llega el ID de un participante
      * @return devuelve un objeto tipo tarea si la encuentra o un null si no
      */
 
-    public Tarea buscadorDeTareas(String fecha, int idTarea){
+    public Tarea buscadorDeTareas(String fecha, int idTarea) {
         for (Tarea t : tarea) {
             if (t.getFecha().equals(fecha) && t.getId() == idTarea) {
                 System.out.println(t);
                 return t;
             }
-        }return null;
+        }
+        return null;
     }
 
     /**
      * Método para obtener la tarea mediante su ID
-     * @param IDparticipante recibe un integer
+     *
+     * @param idTarea recibe un integer
      * @return devuelve un objeto tarea
      */
 
-    public Tarea buscadorDeTareas(int idTarea){
+    public Tarea buscadorDeTareas(int idTarea) {
         for (Tarea t : tarea) {
             if (t.getId() == idTarea) {
                 System.out.println(t);
                 return t;
             }
-        }return null;
+        }
+        return null;
     }
 
-    public List<Tarea> buscarTareasPorUsuario(int idUser, String fecha){
-        List<Tarea>listaEncontrados=new ArrayList();
-        //recorrer las tareas para obetener aquellas cuyo usuario id == idUser
-//
-        return listaEncontrados;
-    }
+    /**
+     *  Metodo para encontrar todas las tareas donde participa un usuario en una fecha indicada
+     * @param idUser  Recibe el ID del usuario
+     * @param fecha   Recibe la fecha de las tareas
+     * @return devuelve una lista de tareas o un null
+     */
 
+    public List<Tarea> buscarTareasPorUsuario(int idUser, String fecha) {
+        List<Tarea> listaEncontrados = new ArrayList();
+        for (Tarea t : tarea) {
+            if (t.getUsuario().getIDusuario() == idUser) {
+                listaEncontrados.add(t);
+            }
+        }
+        if (listaEncontrados.isEmpty()) {
+            return null;
+        } else {
+            return listaEncontrados;
+        }
+    }
 }
--- src/main/java/org/it2022/servicios/GestorTareas.java
+++ src/main/java/org/it2022/servicios/GestorTareas.java
@@ -1,23 +1,24 @@
 package org.it2022.servicios;
 
 import org.it2022.modelo.Tarea;
+import org.it2022.persistencia.PersistenciaTareaInf;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.ArrayList;
 import java.util.List;
 @Named
 public class GestorTareas implements GestorTareasInf {
 
-    // private final List<Tarea> almacenDeTareas = new ArrayList<>();
-    //private repo;
+    @Inject
+    private PersistenciaTareaInf repoTareas;
 
     public void addTarea(Tarea tarea) { //Permite añadir tareas a la lista del GestorTareas
         // this.almacenDeTareas.add(tarea);
     }
 
     public List<Tarea> buscaTareasPorUsuarioYFecha(String fecha, int idUsuario){
-        //return repo.buscarTareasPorUsuario(idUsuario,fecha);
-        return null;
+        return repoTareas.buscadorDeTareas(fecha, idUsuario);
     }
 
 
