5a013148ed441a51f5e38fd4ab62dcb1c0fbbb5a
SergiBF <sboronat04@gmail.com>
2022-07-05 08:30:46 +0200

    Hey2

--- GestorAPIAgenda2022/src/main/java/com/it2022/persistencia/UsuarioManager.java
+++ GestorAPIAgenda2022/src/main/java/com/it2022/persistencia/UsuarioManager.java
@@ -1,11 +1,13 @@
 package com.it2022.persistencia;
 
 import com.it2022.modelo.Usuario;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
+@Repository
 public class UsuarioManager {
     @PersistenceContext
     private EntityManager em;
--- GestorAPIAgenda2022/src/test/java/com/it2022/persistencia/UsuarioManagerTest.java
+++ GestorAPIAgenda2022/src/test/java/com/it2022/persistencia/UsuarioManagerTest.java
@@ -1,14 +1,18 @@
 package com.it2022.persistencia;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+@ComponentScan("com.it2022.persistencia")
 class UsuarioManagerTest {
 
 
     @Test
     void getUsuarios() {
-        new UsuarioManager();
+
     }
 }
